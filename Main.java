1980a063d899df0ce980f332c395f1bc3d4a1fa3
DianaKov <119618395+DianaKov@users.noreply.github.com>
2023-01-04 23:14:20 +0200

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,13 @@
+/* Виводить на екран ПІБ та забезпечує введення рядка (ПІБ) з клавіатури */
+package prog.academy.lesson1;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Kovalevska Diana Vyacheslavivna");
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        System.out.println(str);
+    }
+}
