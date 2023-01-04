0174019e8ab72cc8bdc0c7aeadd774e4d9fd5b49
DianaKov <119618395+DianaKov@users.noreply.github.com>
2023-01-04 23:15:32 +0200

    Create Main2.java

---
+++ Main2.java
@@ -0,0 +1,34 @@
+/* Практичне завдання 2.1*/
+package sample;
+
+/**
+ * A program that print a string
+ * @author Diana Kovalevska
+ * @version 1.0
+ */
+
+public class Main {
+    /**
+     * Start of the program
+     * @param args command line args
+     */
+    public static void main(String[] args) {
+
+        boolean result = false;
+        char code = 'a';
+        byte number = 10;
+        short bigNumber = 336;
+        long bigVal = 99L;
+        int prise = 100;
+        float sum = 3.4f;
+        double pi = 3.14159;
+        System.out.println("result = " + result);
+        System.out.println("code = " + code);
+        System.out.println("number = " + number);
+        System.out.println("bigNumber = " + bigNumber);
+        System.out.println("bigVal = " + bigVal);
+        System.out.println("prise = " + prise);
+        System.out.println("sum = " + sum);
+        System.out.println("pi = " + pi);
+    }
+}
